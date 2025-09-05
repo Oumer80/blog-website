3f1912c963143cb264f54b8ad0c8a46ac0e294ce
Oumer80 <oumersherif123@gmail.com>
2025-09-05 11:33:10 +0000

    f

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { AuthContext } from "./useAuth"
 import { useState } from "react"
-// import axios from 'axios'
+import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
 export const UserProvider = ({children})=>{
     const [user, setUser]=useState(null)
@@ -16,16 +16,36 @@ export const UserProvider = ({children})=>{
             setLoading(false);
         }, []);
 
-    const updateUser=(userData)=>{
-        setUser(userData)
-    }
+    const login = async (email, password) => {
+            try {
+                const response = await axios.post('/api/users/login', { email, password });
+                const userData = response.data;
+                localStorage.setItem('user', JSON.stringify(userData));
+                setUser(userData);
+                navigate('/profile');
+            } catch (error) {
+                console.error(error.response?.data?.message || 'Login failed');
+            }
+        };
+
+    const register = async (name, email, password) => {
+            try {
+                const response = await axios.post('/api/users', { name, email, password });
+                const userData = response.data;
+                localStorage.setItem('user', JSON.stringify(userData));
+                setUser(userData);
+                navigate('/profile');
+            } catch (error) {
+                console.error(error.response?.data?.message || 'Registration failed');
+            }
+        };
 
     const logout = ()=>{
         localStorage.clear()
         setUser(null)
         navigate('/login')
     }
-    const value={user, loading,logout, navigate,updateUser,token}
+    const value={user, loading,logout,login,register, navigate}
     return(
         <AuthContext.Provider value={value}>
            {children}
